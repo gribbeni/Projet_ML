77cb05fd1d9315a59cb2986a52c30a09b0341242
gribbeni <71180877+gribbeni@users.noreply.github.com>
2020-11-18 09:52:38 +0100

    add all files

---
+++ .ipynb_checkpoints/train_eval-checkpoint.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchsampler import ImbalancedDatasetSampler
+import matplotlib.pyplot as plt
+import os
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from functools import reduce
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import confusion_matrix
+
+
+# In[2]:
+
+
+import loaders as l
+import models as m
+
+
+# In[6]:
+
+
+def calc_metrics_v1(net,loader,all_labels,all_predicted,show=True) : 
+
+    for i, data in enumerate(loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            outputs = net(inputs)
+            y_pred_softmax = torch.log_softmax(outputs, dim = 1)
+            _, y_pred_tags = torch.max(y_pred_softmax, dim = 1)
+            all_predicted.extend(np.array(y_pred_tags))
+            all_labels.extend(np.array(labels))
+    
+    f1s=f1_score(all_labels,all_predicted,average='weighted')
+    prec=precision_score(all_labels,all_predicted,average='weighted')
+    rec=recall_score(all_labels,all_predicted,average='weighted')
+    cf_m=confusion_matrix(all_labels,all_predicted,normalize='true')
+    
+    if show ==True : 
+        print('Validation set')
+        print("f1_score",f1s)
+        print("precision",prec)
+        print("recall",rec)
+        print("confusion matrix\n", cf_m)
+        
+    return f1s,prec,rec,cf_m
+
+
+# In[9]:
+
+
+def train_v1(net,criterion,optimizer,epochs,train_loader,valid_loader):
+    
+    all_labels=[]
+    all_predicted=[]
+    all_losses=[]
+    all_accuracies=[]
+    all_f1scores=[]
+    
+    for epoch in range(epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for n_batch,batch in enumerate(train_loader) : 
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = batch
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            all_labels.extend(np.array(labels))
+
+            _, predicted = torch.max(outputs.data, 1)
+            all_predicted.extend(np.array(predicted))
+
+            running_loss += loss.item()
+            if n_batch % 2000 == 1999:    # print every 10 mini-batches
+
+                #peut-être ça ne sert plus---------
+                y_pred_softmax = torch.log_softmax(outputs, dim = 1)
+                _, y_pred_tags = torch.max(y_pred_softmax, dim = 1)
+                outputs=np.array(y_pred_tags)
+                labels=np.array(labels)
+                #---------------------------------
+                all_losses.append(running_loss / n_batch)
+                f1 = calc_metrics_v1(net,valid_loader,all_labels,all_predicted,False)[0]
+                all_f1scores.append(f1)
+                print('[%2d, %2d] loss: %.3f f1_score on validation set : %.3f' %
+                      (epoch + 1, n_batch + 1, running_loss / n_batch,f1))
+
+                running_loss = 0.0
+            
+    
+    return all_labels,all_predicted,all_losses,all_accuracies,all_f1scores
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+
+'''
+train_dir = 'C:/Users/33783/Desktop/start_deep/start_deep/train_images'
+test_dir = 'C:/Users/33783/Desktop/start_deep/start_deep/test_images'
+
+transform = transforms.Compose(
+    [transforms.Grayscale(), 
+     transforms.ToTensor(), 
+     transforms.Normalize(mean=(0,),std=(1,))])
+
+valid_size = 0.2
+batch_size = 32
+
+train_loader,valid_loader,test_loader=l.make_all_loaders(train_dir,test_dir,transform,valid_size,batch_size)
+classes = ('noface','face')
+
+net = m.BaseNet()
+print(net)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+epochs=2
+#Training
+
+all_labels,all_predicted,all_losses,all_accuracies,all_f1scores= train_v1(net,criterion,optimizer,epochs,train_loader,valid_loader)
+            
+plt.plot(all_losses, color='blue')
+plt.plot(all_f1scores, color='red')
+'''
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ loaders.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchsampler import ImbalancedDatasetSampler
+import matplotlib.pyplot as plt
+import os
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from functools import reduce
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+from sklearn.metrics import f1_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import confusion_matrix
+
+
+# In[5]:
+
+
+def make_simplers(train_data,valid_size):
+    
+    num_train = len(train_data)
+    indices_train = list(range(num_train))
+    np.random.shuffle(indices_train)
+    split_tv = int(np.floor(valid_size * num_train))
+    train_new_idx, valid_idx = indices_train[split_tv:],indices_train[:split_tv]
+
+    train_sampler = ImbalancedDatasetSampler(train_data,indices=train_new_idx)
+    valid_sampler = ImbalancedDatasetSampler(train_data,indices=valid_idx)
+    
+    return train_sampler,valid_sampler
+
+def make_train_val_loader(train_dir,transform,valid_size,batch_size) : 
+    
+    train_data = torchvision.datasets.ImageFolder(train_dir, transform=transform)
+    
+    train_sampler,valid_sampler = make_simplers(train_data, valid_size)
+    
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=train_sampler, num_workers=1)
+    valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler, num_workers=1)
+    
+    return train_loader,valid_loader
+
+def make_test_loader(test_dir,transform,batch_size):
+    
+    test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
+    
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=1)
+    
+    return test_loader
+
+def make_all_loaders(train_dir,test_dir,transform,valid_size,batch_size):
+    
+    train_loader,valid_loader = make_train_val_loader(train_dir,transform,valid_size,batch_size)
+    
+    test_loader=make_test_loader(test_dir,transform,batch_size)
+    
+    return train_loader,valid_loader,test_loader
+
+
+# In[6]:
+
+
+#Uncomment to test
+'''
+train_dir = 'C:/Users/33783/Desktop/start_deep/start_deep/train_images'
+test_dir = 'C:/Users/33783/Desktop/start_deep/start_deep/test_images'
+
+transform = transforms.Compose(
+    [transforms.Grayscale(), 
+     transforms.ToTensor(), 
+     transforms.Normalize(mean=(0,),std=(1,))])
+
+valid_size = 0.2
+batch_size = 32
+
+train_loader,valid_loader,test_loader=make_all_loaders(train_dir,test_dir,transform,valid_size,batch_size)
+classes = ('noface','face')
+
+print()
+print('Noface in train data', train_loader.dataset.targets.count(0))
+print('Face in train data',train_loader.dataset.targets.count(1))
+print()
+print('Noface in test data',test_loader.dataset.targets.count(0))
+print('Face in test data',test_loader.dataset.targets.count(1))
+print()
+'''
+
+# In[ ]:
+
+
+
+
